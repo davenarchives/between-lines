cf02f2e432e4697283ebfce985a6083182548762
davenarchives <daventheabyss@gmail.com>
2026-02-08 01:01:20 +0800

    chore: Configure enhanced image optimization

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      images: {
+        sizes: [640, 750, 828, 1080, 1200],
+        formats: ['image/avif', 'image/webp'],
+        minimumCacheTTL: 60
+      }
+    })
   },
   preprocess: vitePreprocess()
 };
